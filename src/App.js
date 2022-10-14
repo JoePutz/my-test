b87bb99621120586beb40b36936924fc9ae224f2
Joseph <josephputz89@gmail.com>
2022-10-14 12:23:42 -0500

    palindrome

--- src/App.js
+++ src/App.js
@@ -17,6 +17,25 @@ function onCheck() {
 
 }
 
+var isPalindrome = function(x) {
+  if (x < 0) {
+      return false
+  }
+  else {
+      let number = x;
+      let reverse = 0;
+      while (number > 0) {
+      reverse = reverse * 10 + number % 10;
+      number = parseInt(number / 10);
+      
+      if( x === reverse) {
+          return true
+      }
+  }
+  // return x === reverse;
+  }
+};
+
 function isPrime(a) {
   if (a === 1 ) {
     return false
