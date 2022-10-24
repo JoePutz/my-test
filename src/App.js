af61c6da7e65de5cc24daf9bcbbe74360a0299c1
Joseph Putz <josephputz89@gmail.com>
2022-10-23 20:49:56 -0500

    findERrorNums

--- src/App.js
+++ src/App.js
@@ -242,6 +242,62 @@ var maxArea = function(height) {
   };
 
 
+  var findErrorNums = function(nums) {
+    let answer = [0, 0]
+    
+    
+    for (i=1; i < nums.length+1; i++) {
+        let count = 0
+        if (nums.indexOf(i) == -1) {
+            answer[1] = i
+        }
+        
+        else {
+            for (j=0; j < nums.length; j++) {
+            if (i == nums[j]) {
+                count ++
+            }
+                if (count == 2) {
+                    answer[0] = i
+                }
+        }
+    }
+    }
+    
+//     
+    
+    return answer
+    // let newNums = nums.uniq
+    
+    
+    // for (i=0; i < nums.length-1; i++) {
+    //     for (j=i+1; j < nums.length; j++) {
+    //         if (nums[i] === nums[j]) {
+    //             answer.push(nums[i])
+    //         }
+    //     }
+    // }
+    // for (i=1; i < nums.length+1; i++) {
+    //     if (nums.includes(i)) {
+    //         console.log("do nothing")
+    //     }
+    //     else {
+    //         answer.push(i)
+    //     }
+    // }
+    // for (i = 1; i < nums.length+1; i++) {
+    //     for (j=0; j < nums.length; j++) {
+    //         if (i !== nums[j]) {
+    //             answer.push(i)
+    //         }
+    //     }
+    // }
+    
+    return answer
+    
+};
+
+
 
 
 
