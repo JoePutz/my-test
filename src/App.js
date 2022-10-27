2d8b14772cc09f7769eec1fc605409058e954c9d
Joseph Putz <josephputz89@gmail.com>
2022-10-27 12:08:44 -0500

    largest overlap

--- src/App.js
+++ src/App.js
@@ -318,6 +318,26 @@ var arrayStringsAreEqual = function(word1, word2) {
 };
 
 
+var largestOverlap = function(A, B) {
+  var len = A.length;
+ console.log(A.length)
+var res = 0;
+var tmp = 0;
+for (var i = 1 - len; i < len; i++) {
+ for (var j = 1 - len; j < len; j++) {
+   tmp = 0;
+   for (var k = 0; k < len; k++) {
+     for (var m = 0; m < len; m++) {
+       if (B[k][m] === 1 && A[k + i] && A[k + i][m + j] === 1) tmp++;
+     }
+   }
+   res = Math.max(res, tmp);
+ }
+}
+return res;
+};
+
+
 
 
 
