50c55fd16f9a3000c3954736066dc55b504b6be7
Joseph Putz <josephputz89@gmail.com>
2023-02-03 13:01:43 -0600

    find substrings

--- src/App.js
+++ src/App.js
@@ -1103,6 +1103,49 @@ var divide = function(dividend, divisor) {
 //This uses bit math (in binary) to do things for me.
 //>> and << are binary methods
 
+var findSubstring = function(s, words) {
+  const answer = []
+  if (s === null || s.length === 0 || words === null || words.length === 0) return answer
+
+  const wordCount = words.reduce((a, b) => {
+      a[b] = (a[b] + 1) || 1;
+      return a;
+  }, {})
+
+  const wordLength = words[0].length;
+  const wordArrayLength = wordLength * words.length;
+
+  for (i = 0; i <= s.length - wordArrayLength; i++) {
+      
+      let current = s.substring(i, i + wordArrayLength);
+      // Map to store each word of the substring
+      const wordMap = {};
+      // Index to loop through the words array
+      let index = 0;
+      // Index to get each word in the current
+      let j = 0;
+      // Loop through each word of the words array
+      while (index < words.length) {
+          // Divide the current string into strings of length of
+          // each word in the array
+          const part = current.substring(j, j + wordLength);
+          // Put this string into the wordMap
+          wordMap[part] = (wordMap[part] + 1) || 1;
+          // Update j and index
+          j += wordLength;
+          index++;
+      }
+      // At this point compare the maps
+      let a = JSON.stringify(Object.entries(wordCount).sort());
+      let b = JSON.stringify(Object.entries(wordMap).sort());
+      if (a === b) {
+          answer.push(i);
+      }
+  }
+  return answer;  
+};
+//A weird one, about finding combinations of words in a long string. 
+
 const [brettNum, setBrettNum] = useState(0)
 const [oliverNum, setOliverNum] = useState(0)
 const [danNum, setDanNum] = useState(0)
