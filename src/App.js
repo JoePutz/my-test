a54c40bafe79b62ad562c64ca6c56b56ea2da042
Joseph Putz <josephputz89@gmail.com>
2023-03-13 13:53:06 -0500

    jump function

--- src/App.js
+++ src/App.js
@@ -1786,6 +1786,23 @@ var mySqrt = function(x) {
 };
 //So an wasy one about finding the square root. 
 
+var jump = function(nums) {
+  var len = nums.length;
+  var step = 0;
+  var now = 0;
+  var max = 0;
+
+  for (var i = 0; i < len - 1; i++) {
+    max = Math.max(max, i + nums[i]);
+    if (i === now) {
+      step++;
+      now = max;
+    }
+  }
+
+  return step;
+};
+
 
 const [brettNum, setBrettNum] = useState(0)
 const [oliverNum, setOliverNum] = useState(0)
