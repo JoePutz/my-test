f6cea97670369b078ae12dce488f566f6249c3f4
Joseph Putz <josephputz89@gmail.com>
2023-03-17 13:35:44 -0500

    steps fibonnacci

--- src/App.js
+++ src/App.js
@@ -1803,6 +1803,19 @@ var jump = function(nums) {
   return step;
 };
 
+var climbStairs = function(n) {
+  if (n <= 1) {
+      return 1
+  }
+  let fib  = [1, 1]
+  for (i = 2; i <= n; i++) {
+      amount = fib[i-1] + fib[i-2]
+      fib.push(amount)
+  }
+  return fib[n]
+};
+//Turns out the pattern of increasing steps is just the fibonnacci sequence, who knew?
+
 
 const [brettNum, setBrettNum] = useState(0)
 const [oliverNum, setOliverNum] = useState(0)
